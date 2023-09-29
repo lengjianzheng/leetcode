d4710db9c55e2387f6e3a6215d8585d0c2b3b6a7
Leng Jianzheng <jianzheng.leng@shopee.com>
2023-09-29 22:15:42 +0800

    change log

--- STL/map.cpp
+++ STL/map.cpp
@@ -5,15 +5,20 @@
 using namespace std;
 
 // 简单的增删改查，遍历
+// 注意: 
 // 1, 获得值的时候  std::cout << myMap["haha"]; 会在map里插入一个键值对("haha", 0)
 int main() {
-    map<string, int> myMap; // 初始化
-    myMap["abc"] = 123; // 插入
-    myMap.insert(std::make_pair<string, int>("def", 456)); // 插入
-    myMap.erase("abc"); // 删除
-    std::cout << myMap["def"] << ", " << myMap["haha"] << endl; // 获取值
+    // 1, 初始化
+    map<string, int> myMap; 
+    // 2, 插入
+    myMap["abc"] = 123; 
+    myMap.insert(std::make_pair<string, int>("def", 456));
+    // 3, 删除
+    myMap.erase("abc");
+    // 4, 查找, 获得值的时候  std::cout << myMap["haha"]; 会在map里插入一个键值对("haha", 0)
+    std::cout << myMap["def"] << ", " << myMap["haha"] << endl;
     myMap["def"] = 777;
-    // 遍历
+    // 5, 遍历
     for (auto it = myMap.begin(); it != myMap.end(); it++) {
         cout << "key:" << it->first << ", value:" << it->second << endl;
     }
